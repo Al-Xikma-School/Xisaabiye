754c179f6776e77feae01667cfce2f6cbe145b96
farxaan109 <farxaan109@gmail.com>
2026-05-18 15:03:19 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'xisaabiye-v2';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
